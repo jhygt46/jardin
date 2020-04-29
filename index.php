c939791960106ace2bb4f3c5f44cd49d0bbfd8e3
jhygt46 <infolifeoperacional@gmail.com>
2020-04-29 18:31:18 -0400

    aa

--- index.php
+++ index.php
@@ -223,6 +223,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165015206-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-165015206-1');
+        </script>
+
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Jardin Infantil y Sala Cuna Valle Encantado</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
