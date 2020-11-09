28852b50dd32855081e5b7b8ccaa9e679b4167a9
jhygt46 <infolifeoperacional@gmail.com>
2020-11-09 00:49:53 -0300

    aa

--- send/index.php
+++ send/index.php
@@ -12,35 +12,25 @@ if($_POST["accion"] == "enviar" && $_POST["nombre"] != "" && $_POST["correo"] !=
 	$send['mensaje'] = $_POST["mensaje"];
     $send['tipo'] = 3;
     
-    $txt = $_POST["nombre"]." // ".$_POST["correo"]." // ".$_POST["telefono"]." // ".$_POST["mensaje"]."\n";
-
-    file_put_contents("contactos.txt", $txt, FILE_APPEND | LOCK_EX);
-    $info['op'] = 2;
-    
 	$ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'http://34.121.26.254/mail_jardin');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($send));
     if(!curl_errno($ch)){
-        $info['mail'] = json_decode(curl_exec($ch));
+        $resp_email = json_decode(curl_exec($ch));
         curl_close($ch);
-    }
-    /*
-    if($resp_email->{'op'} == 1){
-        // ALERTAR ERROR //
-        $info['op'] = 1;
-    }
-    if($resp_email->{'op'} != 1){
-        // ALERTAR ERROR //
-        $info['op'] = 2;
-        $info['msj'] = "Error1: Mensaje no fue enviado";
-    }
+        if($resp_email->{'op'} == 1){
+            $info['op'] = 1;
+        }
+        if($resp_email->{'op'} == 2){
+            $info['op'] = 2;
+            $info['msj'] = "Error: Mensaje no fue enviado";
+        }
 	}else{
 		$info['op'] = 2;
-		$info['msj'] = "Error2: Mensaje no fue enviado";
+		$info['msj'] = "Error: Mensaje no fue enviado";
     }
-    */
-   
+    
 }else{
     $info['op'] = 2;
     $info['msj'] = "Error: Debe Completar los Campos";
